f247988f2f72320138742d2ccd57dbfdeddbb9d0
alencarol <alencarolina25@gmail.com>
2022-05-13 21:30:15 -0300

    Implemantando conexão com api pela busca por cidade

--- clima-app/pages/search.js
+++ clima-app/pages/search.js
@@ -1,10 +1,43 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
+import api from '../server/api';
+import { useState, useEffect } from 'react';
+
 const SearchScreen = () => {
+    const [cityClimate, setCityClimate] = useState({})
+
+    const getRequest = async () => {
+        api.get("/weather?key=44d09c68&city_name=Crato,ce")
+            .then((response) => {
+                console.log("GET Response")
+                console.log(response.data);
+                setCityClimate(response.data.results)
+                console.log(cityClimate)
+            })
+            .catch(function (error) {
+                console.log("Error ao carregar dados cidade",error);
+            });
+
+    }
+    useEffect(() => {
+        getRequest()
+
+    }, [])
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Search!</Text>
-        </View>
+        <ScrollView >
+            <View>
+                <Text>{cityClimate.description}</Text>
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
 
 export default SearchScreen
