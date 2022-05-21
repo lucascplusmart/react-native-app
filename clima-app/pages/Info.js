e2624616036119d1e9f7910fec4870b2ace901e0
alencarol <alencarolina25@gmail.com>
2022-05-21 10:50:27 -0300

    Configurando variáveis de ambientes para as keys da API

--- clima-app/pages/Info.js
+++ clima-app/pages/Info.js
@@ -3,29 +3,10 @@ import { StyleSheet, ScrollView, Text, View } from 'react-native';
 
 
 const InfoScreen = () => {
-    async function getLocation () {
-        let {locationStatus} = await Location.requestForegroundPermissionsAsync()
-
-        if (locationStatus !== 'granted'){
-            console.log("Acesso negado")
-        }else{
-            console.log("Acesso permitido")
-            let location = await Location.getCurrentPositionAsync({});
-            setGeoLocation(location.coords);
-            console.log(location)
-            console.log(location.coords)            
-        }
-    }
-
-    useEffect(() => {
-        getLocation()
-
-    }, [])
-
 
     return (
             <View  style={styles.container}  >
-                <Text>Informações do App</Text>
+                <Text>Esse aplicativo foi desenvolvido visado a obtenção da nota parcial da disciplina de PSWI...</Text>
             </View>
     );
 }
